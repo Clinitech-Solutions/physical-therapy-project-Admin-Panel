ba1a531f60ba4abd583519b252719d864b907ea5
Clinitech-Solutions <clinitechsolutions65@gmail.com>
2026-09-14 01:06:14 +0300

    feat: add app.config.ts with core application providers for routing, HTTP, PrimeNG, and localization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
@@ -15,7 +15,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), 
     provideHttpClient(),
     provideAnimations(),
     providePrimeNG({
